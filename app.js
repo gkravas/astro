a57b6fa9b5ae550ba12fd3be38ed240d2c6ba996
George Kravas <gkravas@gmail.com>
2017-11-11 18:38:42 +0200

    logging fixes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import 'babel-polyfill'
 const config = require('./config/index');
 
 import { logger } from './helpers/logger';
+const morgan = require('morgan')
+
 const compression = require('compression')
 const express = require('express');
 const expressJwt = require('express-jwt');  
@@ -14,12 +16,28 @@ const graphqlSchema = require('./graphqlschema')(models, logger);
 const authController = require('./controllers/authController');
 const emailService = require('./services/emailService')(config, models, logger);
 const cors = require('cors');
+const mung = require('express-mung');
 
 const app = express().use('*', cors());;
 
+const format = ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" req.body :reqBody res.body :resBody';
+
+morgan.token('reqBody', function (req, res) { 
+    return JSON.stringify(req.body);
+});
+morgan.token('resBody', function (req, res) { 
+    return JSON.stringify(res.logBody);
+});
 app.use(compression())
+app.use(mung.json(
+    function transform(body, req, res) {
+        res.logBody = body
+        return body;
+    }
+));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan(morgan.compile(format), { stream: logger.stream }));
 //app.use(require('serve-static')(__dirname + '/../../public'));
 //app.use(require('cookie-parser')());
 //app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
--- helpers/logger.js
+++ helpers/logger.js
@@ -3,7 +3,8 @@ import { createLogger, format, transports } from 'winston';
 const isProduction = process.env.ENV === 'production'
 
 export const logger = createLogger({
-    level: isProduction ? 'info' : 'debug'
+    level: isProduction ? 'info' : 'debug',
+    exitOnError: false
 });
 
 if (!isProduction) {
@@ -12,7 +13,16 @@ if (!isProduction) {
     }));
 } else {
     logger.add(new transports.File({
+        handleExceptions: true,
+        maxsize: 5242880, //5MB
+        maxFiles: 5,
         filename: './logs/log.log',
         format: format.combine(format.timestamp(), format.json()),
     }));
 }
+
+logger.stream = {
+    write: function(message, encoding){
+        logger.info(message);
+    }
+};
