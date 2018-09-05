0074abdfef525dd6bf16486a3a11b17a0165c194
George Kravas <gkravas@gmail.com>
2018-09-05 12:16:25 +0300

    point firebase messages to topic daily

--- src/schedulers.js
+++ src/schedulers.js
@@ -16,6 +16,9 @@ const texts = [
     "Άμεσες προβλέψεις για το μέλλον, από τους καλύτερους αστρολόγους!",
     "Ανακάλυψε τι σου επιφυλάσσει το μέλλον!"
 ];
+
+const topic = 'daily';
+
 var rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [0, new schedule.Range(2, 6)];
 rule.hour = 8;
@@ -23,16 +26,13 @@ rule.minute = 0;
 rule.tz = 'Europe/Athens';
 
 var j = schedule.scheduleJob(rule, function(){
-  
-    // This registration token comes from the client FCM SDKs.
-    var registrationToken = 'dmu4c2rXQZs:APA91bHGc-j87FMMqyLebuQXy4fPTqtw2Gs-Ok-Kc0-HmHKsL7E4WhGhMfIhFTbqh4TyscK1VYvlrH8s9lRHzrLeY2SRjIr3IXzvyveKq7AIQdf_S68iCK17SHSBOgP9Elrgu_c2kl5H8WFT7ADLDkX9knG7eaP4YA';
 
     // See documentation on defining a message payload.
     var message = {
         notification: {
             body: texts[Math.floor(Math.random() * texts.length)]
         },
-        token: registrationToken
+        topic: topic
     };
 
     // Send a message to the device corresponding to the provided
